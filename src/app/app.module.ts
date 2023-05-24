d82d2e52e7388257c361053f031a90097466d1c7
Fran Linde Blázquez <franlindebl@gmail.com>
2023-05-24 14:18:36 +0200

    Removed SW

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { PokemonListComponent } from 'src/components/PokemonList/pokemon-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PokemonItemComponent } from 'src/components/PokemonItem/pokemon-item.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+// import { ServiceWorkerModule } from '@angular/service-worker';
+// import { isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonDetal } from 'src/components/PokemonDetail/pokemon-detail.component';
 
@@ -22,12 +23,12 @@ import { PokemonDetal } from 'src/components/PokemonDetail/pokemon-detail.compon
     AppRoutingModule,
     HttpClientModule,
     CommonModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:2000'
-    }),
+    // ServiceWorkerModule.register('ngsw-worker.js', {
+    //   enabled: !isDevMode(),
+    //   // Register the ServiceWorker as soon as the application is stable
+    //   // or after 30 seconds (whichever comes first).
+    //   registrationStrategy: 'registerWhenStable:2000'
+    // }),
   ],
   providers: [],
   bootstrap: [AppComponent]
