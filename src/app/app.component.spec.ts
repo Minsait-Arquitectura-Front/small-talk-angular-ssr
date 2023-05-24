155a4e5ee11914c7afcad46313b3026e6d0b4063
Fran Linde Blázquez <franlindebl@gmail.com>
2023-05-24 14:30:17 +0200

    Change app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pwa-small-talk-angular'`, () => {
+  it(`should have as title 'small-talk-angular-ssr'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pwa-small-talk-angular');
+    expect(app.title).toEqual('small-talk-angular-ssr');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pwa-small-talk-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('small-talk-angular-ssr app is running!');
   });
 });
